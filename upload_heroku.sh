94c82f3923289f9b24d4fa517ae9809b32fe7563
Kelvin Kramp <k.h.kramp@gmail.com>
2022-03-31 14:45:12 +0200

    working booking workflow

--- upload_heroku.sh
+++ upload_heroku.sh
@@ -3,7 +3,3 @@ pip freeze > requirements.txt
 git add .
 git commit -m "update"
 git push heroku master
-#heroku pg:reset --confirm urbankizbookfeedback
-#heroku run python database_setup.py
-#heroku logs -n 20
-#heroku run python start_test.py
