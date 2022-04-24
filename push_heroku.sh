c442747aeb0cf635611baf6122ccd29b2bff0bfa
Kelvin Kramp <k.h.kramp@gmail.com>
2022-04-24 03:36:58 +0200

    update

--- push_heroku.sh
+++ push_heroku.sh
@@ -3,6 +3,9 @@ pip freeze > requirements.txt
 git add .
 git commit -m "update"
 git push heroku master
-heroku run python start_test.py
+heroku run python database_setup.py
+#heroku run python start_test.py
+
+#heroku pg:reset --confirm urbankizbookfeedback
 #heroku run python database_setup.py
-#heroku logs -n 20
+heroku logs -n 20
